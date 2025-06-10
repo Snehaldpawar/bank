6802955bc303eeb8b28158604d4ec758d55deb52
Snehaldpawar <160159667+Snehaldpawar@users.noreply.github.com>
2025-06-10 15:28:30 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 
